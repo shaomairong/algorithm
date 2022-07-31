a4b796484a7f603b43c2f973c08f4864e7a47bdc
shaomairong <shaomairong@yeah.net>
2022-07-31 16:43:48 +0800

    递归和非递归中序遍历二叉树

--- lc94二叉树的中序遍历.cpp
+++ lc94二叉树的中序遍历.cpp
@@ -1,25 +1,34 @@
-  void inOrderTraversalRecursion(Node* x){
-        if(x == nullptr) return;
-        inOrderTraversalRecursion(x->left);
-        cout<<x->val<<endl;
-        inOrderTraversalRecursion(x->right);
-    }
-	 void inOrderTraversalIteration(Node* x){//二叉树中序遍历迭代法实现
-        if(x == nullptr) return;
-        cout<<"inOrderTraversalIteration"<<endl;
-        stack<Node* > s;
-        Node* cur = x;
-        Node* tmp = nullptr;
-        //栈不为空或者cur不为空，那么循环继续
-        while (!s.empty() || cur){
-            if(cur){ //以cur为头节点的树，整条左边进栈(包括cur这个根节点),直到遇到cur的左边为nullptr结束
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root==nullptr) return {};
+        stack<TreeNode*> s;
+        TreeNode* cur = root;
+        //栈非空 或者 cur不为空 就继续循环
+        while(!s.empty() || cur){
+            if(cur){
+    //以cur为头节点的树，整条左边进栈(包括cur这个根节点),直到遇到cur的左边为nullptr结束
                 s.push(cur);
                 cur = cur->left;
             }else {
-                cur = s.top(); //从栈中弹出cur,经过上面if判断cur的左边是nullptr,访问cur后，让cur=cur->right;
-                s.pop();
-                cout<<cur->val<<endl;
+     //从栈中弹出cur,经过上面if判断cur的左边是nullptr,访问cur后，让cur=cur->right;
+                cur = s.top();s.pop();
+                ans.push_back(cur->val);
                 cur = cur->right;
             }
         }
+        return ans;
     }
+    vector<int> inorderTraversal1(TreeNode* root) {
+        if(root==nullptr) return {};
+        in(root);
+        return ans;
+    }
+    void in(TreeNode* x){
+        if(x == nullptr) return;
+        in(x->left);
+        ans.push_back(x->val);
+        in(x->right);
+    }
+};
